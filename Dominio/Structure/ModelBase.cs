bfc35a4f66b827cb88f8f425e2828e0b9e9fda0e
Pedro Barros <82074537+pedrobarros01@users.noreply.github.com>
2024-11-05 22:35:30 -0300

    hotfix: envio do mapeamento do usuario

--- Dominio/Structure/ModelBase.cs
+++ Dominio/Structure/ModelBase.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
